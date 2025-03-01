a8f52b5758a61061b365bed0540f99f8c9ec8d7b
farid.ansari <farid.ansari@gmail.com>
2025-03-01 02:44:11 -0800

    second commit

---
+++ src/parentnode.py
@@ -0,0 +1,25 @@
+from htmlnode import HTMLNode
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag=None, children=None, prop=None):
+        self.tag = tag
+        self.children=children
+        self.prop = prop
+        super().__init__(tag=self.tag, value=None, children=self.children, prop=self.prop)
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("All Parent nodes must have a tag")
+        if self.children == None:
+            raise ValueError("All Parent nodes must have children")
+
+        # Start with the opening tag of the parent
+        html_content = f"<{self.tag}>"
+
+        # Iterate over the children (recursive)
+        for child in self.children:
+            html_content += child.to_html()
+
+        # Add the closing tag of the parent
+        html_content += f"</{self.tag}>"
+
+        return html_content
---
+++ src/test_parentnode.py
@@ -0,0 +1,42 @@
+import unittest
+
+from parentnode import ParentNode
+from leafnode import LeafNode
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_to_html_with_multi_children(self):
+        child_node_a = LeafNode("span", "span_content")
+        child_node_b = LeafNode("p", "paragraph_content")
+        child_node_c = LeafNode("button", "submit")
+        parent_node = ParentNode("div", [child_node_a, child_node_b, child_node_c])
+        self.assertEqual(parent_node.to_html(), "<div><span>span_content</span><p>paragraph_content</p><button>submit</button></div>")
+
+        
+    def test_to_html_with_multi_grandchildren(self):
+        child_node_a = LeafNode("span", "span_content")
+        grandchild_node_b1 = LeafNode("i", "italic_text")
+        grandchild_node_b2 = LeafNode("b", "bold_text")
+        child_node_b = ParentNode("p", [grandchild_node_b1, grandchild_node_b2])
+        child_node_c = LeafNode("button", "submit")
+        parent_node = ParentNode("div", [child_node_a, child_node_b, child_node_c])
+        self.assertEqual(parent_node.to_html(), "<div><span>span_content</span><p><i>italic_text</i><b>bold_text</b></p><button>submit</button></div>")
+
+    def test_to_html_without_children(self):
+        with self.assertRaises(ValueError) as context:
+            parent_node = ParentNode("div", children=None)
+            parent_node.to_html()
+            self.assertEqual(str(context.exception), "All Parent nodes must have children")
