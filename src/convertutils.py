7df3b1148b7d8f129e5fe7b6d4005e9644efd86e
farid.ansari <farid.ansari@gmail.com>
2025-03-02 04:10:44 -0800

    Still working on converting nodes to html

--- src/convertutils.py
+++ src/convertutils.py
@@ -8,43 +8,100 @@ import pprint
 
 def markdown_to_html_node(markdown):
     markdown_blocks = markdown_to_blocks(markdown)
-    node_list = []
+    top_html_node = ParentNode("div", None, None)
+    block_html_nodes = []
+
     for block in markdown_blocks:
         block_type = block_to_block_type(block)
-        block_text = ""
+        block_html_node = map_block_to_html_node(block_type, block)
+        block_body_text = strip_markup(block, block_type)
+        children_nodes = text_to_textnodes(block_body_text)
+        
+        child_html_nodes = []
+        for child in children_nodes:
+            child_html_nodes.append(text_node_to_html_node(child))
+
+        block_html_node.children = child_html_nodes
+        block_html_nodes.append(block_html_node)
+
+    top_html_node.children = block_html_nodes    
+    return top_html_node
+
+def map_block_to_html_node(block_type, block):
         match (block_type):
             case BlockType.P:
                 node = ParentNode("p", None, None)
-                block_text = block
             case BlockType.HEADING:
-                node = ParentNode("h1", None, None)
-                block_text = block.lstrip("#")
+                node = ParentNode(map_block_header_tag(block), None, None)
             case BlockType.CODE_BLOCK:
                 node = ParentNode("code", None, None)
-                block_text = block[2:-2]
             case BlockType.QUOTE_BLOCK:
                 node = ParentNode("blockquote", None, None)
-                block_text = block.strip(">")
             case BlockType.UL:
                 node = ParentNode("UL", None, None)
-                block_text = block.strip("- ")
             case BlockType.OL:
                 node = ParentNode("OL", None, None)
-                block_text = block.strip("1. ")
             case _:
                 node = node = ParentNode("p", None, None)
-        node_list.append(node)
-        text_to_children(block)
+        return node
 
-    # print("\n")
-    # pprint.pp(node_list)
-    # print("\n")
-        # pprint.pp("**** LOOOP *****")
-        # pprint.pp(block)
-        # pprint.pp(block_type)
+def strip_markup(block, block_type):
+    block_body_text = ""
+    match (block_type):
+        case BlockType.P:
+            block_body_text = block
+        case BlockType.HEADING:
+            block_body_text = block.lstrip("#")
+        case BlockType.CODE_BLOCK:
+            block_body_text = block[3:-3]
+        case BlockType.QUOTE_BLOCK:
+            block_body_text = split_and_strip(block, ">", False, "<BR/>")
+        case BlockType.UL:
+            block_body_text = split_and_strip(block, "- ", False, "<BR/>", "li")
+        case BlockType.OL:
+            block_body_text = split_and_strip(block, ".", True, "<BR/>",  "li")
+        case _:
+            raise Exception("Did not find valid block type!")
+    return block_body_text
 
-def text_to_children(text):
-    print("THE CHIDREL!!!!!!!")
-    print(text)
+def split_and_strip(block, markup, counterType=False, newLine="\n", multi_line_tag = None):
+    multi_lines = block.split("\n")
+    new_lines = []
+    
+    for line in multi_lines:
+        if (counterType ==  False):
+            current_line = line.replace(markup, "")
+            new_lines.append(current_line)
+        else:
+            current_line = line.split(markup, 1)[1]
+            new_lines.append(current_line)
+
+    if multi_line_tag == None:
+        return f"{newLine}".join(new_lines)
+    else:
+        new_lines_2 = []
+        for line in new_lines:
+            new_lines_2.append(f"<{multi_line_tag}>{line}</{multi_line_tag}>")
+        return "".join(new_lines_2)
+        
+def map_block_header_tag(block):
+    if block.startswith("######"):
+        return "h6"
+    elif block.startswith("#####"):
+        return "h5"
+    elif block.startswith("####"):
+        return "h4"
+    elif block.startswith("###"):
+        return "h3"
+    elif block.startswith("##"):
+        return "h2"
+    elif block.startswith("#"):
+        return "h1"
+    else:
+        raise Exception("This is not a header block!")
+    
+    pass
+    #print("THE CHIDREL!!!!!!!")
+    #print(text)
     #text_nodes = text_to_textnodes(text)
     #pprint.pp(text_nodes)
--- src/test_convertutils.py
+++ src/test_convertutils.py
@@ -3,16 +3,44 @@ from convertutils import *
 import pprint
 
 
-# class TestConvertUtils(unittest.TestCase):
-#     def test_paragraphs(self):
-#         md = """This is **bolded** paragraph
-# text in a p
-# tag here
+class TestConvertUtils(unittest.TestCase):
+    def test_paragraphs(self):
+        md = """This is **bolded** paragraph
+text in a p
+tag here
 
 # This is another paragraph with _italic_ text and `code` here"""
-#         node = markdown_to_html_node(md)
+        node = markdown_to_html_node(md)
 
-#     def test_codeblock(self):
-#         md = """``` This is text that _should_ remain 
-# the **same** even with inline stuff ```"""
-#         node = markdown_to_html_node(md)
+    def test_codeblock(self):
+        md = """``` This is text that _should_ remain 
+the **same** even with inline stuff ```"""
+        node = markdown_to_html_node(md)
+
+    def test_heading1(self):
+        md = """### This is a Level3 Header
+with multi-line"""
+        node = markdown_to_html_node(md)
+
+    def test_heading6(self):
+        md = """###### This is a Level6 Header
+with multi-line"""
+        node = markdown_to_html_node(md)
+
+    def test_quoteblock(self):
+        md = """>Quote Line 1 
+>Quote Line 2
+>Quote Line 3"""
+        node = markdown_to_html_node(md)
+
+    def test_ul(self):
+        md = """- Line A 
+- Line B
+- Line C"""
+        node = markdown_to_html_node(md)
+
+    def test_ol(self):
+        md = """1.Line 1 
+2.Line 2
+3.Line 3"""
+        node = markdown_to_html_node(md)                
