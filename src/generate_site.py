935acdb602cb61d4ec9bba0d3b88b513e118d1a3
farid.ansari <farid.ansari@gmail.com>
2025-03-02 21:43:45 -0800

    created deploy configuration and changes

--- src/generate_site.py
+++ src/generate_site.py
@@ -14,22 +14,23 @@ def copy_files_static_to_public(source_folder, dest_folder):
     
     pubic_folder_files = os.listdir(dest_folder)
 
-#Delete Files
+    #Delete Files
+    print("\nDeleting existing files in public folder...")
+
     for item in pubic_folder_files:
         file_path = os.path.join(dest_folder, item)
         if os.path.isfile(file_path):
-            print(f"\nDeleting file: {file_path}")
+            print(f"Deleting file: {file_path}")
             os.remove(file_path)
         elif os.path.isdir(file_path):
-            print(f"Deleting folder and contents: {file_path}")
+            print(f"Deleting folder and all sub-folders: {file_path}")
             shutil.rmtree(file_path)
 
 #Copy files
-    print(f"Copying files from {source_folder} to {dest_folder}")
+    print(f"\nCopying all files from {source_folder} to {dest_folder}...")
     shutil.copytree(source_folder, dest_folder, dirs_exist_ok=True)
 
 def extract_title(content):
-    print(f"Reading Title from File:" )
 
     if len(content) == 0:
         raise Exception("No content provided")
@@ -43,11 +44,11 @@ def extract_title(content):
         raise Exception("No level 1 header found at start!")
     
     header_text = first_line.lstrip("#").strip()
-    print(f"Found Title: {header_text}")
+    print(f"Found file title: {header_text}\n")
     return header_text
 
-def generate_page(from_path, template_path, dest_path):
-    print(f"\nGenerating file from {from_path} to {dest_path} using {template_path}")
+def generate_page(from_path, template_path, dest_path, basepath):
+    print(f"Generating file from {from_path} --> {dest_path} using {template_path}")
     
     
     markdown_file = open(from_path)
@@ -62,14 +63,14 @@ def generate_page(from_path, template_path, dest_path):
     html_content = html_node.to_html()
 
     formated_content_title = template_content.replace("{{ Title }}", file_title)
-    formated_content_title = formated_content_title.replace("{{ Content }}", html_content)
+    formated_content_body = formated_content_title.replace("{{ Content }}", html_content)
+    formated_content_body_server = formated_content_body.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
 
     new_html_file = open(f"{dest_path}", "x")
-    new_html_file.write(formated_content_title)
+    new_html_file.write(formated_content_body_server)
     new_html_file.close()
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
-    print("\nStarting to generate pages for site....")
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     if not os.path.exists(dir_path_content):
         raise Exception("source path does not exist!!")
     
@@ -87,18 +88,15 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         template_file = os.path.join(template_path, "template.html")
         if os.path.isfile(file_path):
             print(f"Found file: {file_path}")
-            generate_page(file_path, template_file, dest_path)
+            generate_page(file_path, template_file, dest_path, basepath)
         elif os.path.isdir(file_path):
             print(f"Found folder: {file_path}")
             dir_path_content_next = file_path
             dest_dir_path_next = os.path.join(dest_dir_path, item)
+            print(f"Creating destination folder: {dest_dir_path_next}")
             os.mkdir(dest_dir_path_next)
-            generate_pages_recursive(dir_path_content_next, template_path, dest_dir_path_next)
-        
+            generate_pages_recursive(dir_path_content_next, template_path, dest_dir_path_next, basepath)
     return None
-
-    pprint.pp(content_folder_files)
-    
 
 
     
--- src/main.py
+++ src/main.py
@@ -1,15 +1,23 @@
-import os
+import sys
 
 from generate_site import *
 
-DEFAULT_SOURCE_STATIC_FOLDER = "./static"
-DEFAULT_DEST_PUBLIC_FOLDER = "./public"
-DEFAULT_SOURCE_CONTENT_FOLDER = "./content"
-DEFAULT_TEMPLATE_PATH = "."
+basepath = "/"
+if len(sys.argv) > 1:
+    basepath = sys.argv[1]
+
+
+DEFAULT_SOURCE_STATIC_FOLDER = f"./static"
+DEFAULT_DEST_PUBLIC_FOLDER = f"./docs"
+DEFAULT_SOURCE_CONTENT_FOLDER = f"./content"
+DEFAULT_TEMPLATE_PATH = f"."
+
 
 def main():
     print("Starting Static Site Generator")
+    print(f"Base HTTP server basepath= {basepath}")
     copy_files_static_to_public(DEFAULT_SOURCE_STATIC_FOLDER, DEFAULT_DEST_PUBLIC_FOLDER)
-    generate_pages_recursive(DEFAULT_SOURCE_CONTENT_FOLDER, DEFAULT_TEMPLATE_PATH, DEFAULT_DEST_PUBLIC_FOLDER )
+    print("\nStarting to generate pages for site....")
+    generate_pages_recursive(DEFAULT_SOURCE_CONTENT_FOLDER, DEFAULT_TEMPLATE_PATH, DEFAULT_DEST_PUBLIC_FOLDER, basepath )
 
 main()
